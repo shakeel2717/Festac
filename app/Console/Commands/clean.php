d3036901c2f4e5d8d46153f2bf3676ca8a6d89d3
Shakeel Ahmad <shakeel2717@gmail.com>
2021-12-16 17:32:24 +0500

    make clean command created

---
+++ app/Console/Commands/clean.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
+
+class clean extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:clean';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to clean the data from database and cache';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Artisan::call('migrate:fresh');
+        Artisan::call('cache:clear');
+        Artisan::call('config:clear');
+        Artisan::call('view:clear');
+        Artisan::call('route:clear');
+
+        // creating a admin user
+        $admin = new User();
+        $admin->name = 'Admin';
+        $admin->email = 'admin@admin.com';
+        $admin->password = Hash::make('asdfasdf');
+        $admin->role = 'admin';
+        $admin->code = generate_user_code('A');
+        $admin->save();
+
+        // creating a user
+        $user = new User();
+        $user->name = 'User';
+        $user->email = 'shakeel2717@gmail.com';
+        $user->password = Hash::make('asdfasdf');
+        $user->role = 'user';
+        $user->code = generate_user_code('U');
+        $user->save();
+
+        // creating a seller
+        $seller = new User();
+        $seller->name = 'Seller';
+        $seller->email = 'basharat@gmail.com';
+        $seller->password = Hash::make('asdfasdf');
+        $seller->role = 'seller';
+        $seller->code = generate_user_code('S');
+        $seller->save();
+        
+        return Command::SUCCESS;
+    }
+}
