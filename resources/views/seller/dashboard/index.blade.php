4ed5185901599086c3eff26131d6ace40f3d4fb2
Shakeel Ahmad <shakeel2717@gmail.com>
2021-12-19 22:14:33 +0500

    Seller Dashboard Recent Transaction

--- resources/views/seller/dashboard/index.blade.php
+++ resources/views/seller/dashboard/index.blade.php
@@ -75,7 +75,6 @@
                                     <i class="tio-chevron-right tio-lg text-white text-hover-primary"></i>
                                 </div>
                             </div>
-                            <!-- End Row -->
                         </div>
                     </a>
                 </div>
@@ -95,12 +94,104 @@
                                     <i class="tio-chevron-right tio-lg text-white text-hover-primary"></i>
                                 </div>
                             </div>
-                            <!-- End Row -->
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-12">
+                    <a class="card bg-primary card-hover-shadow shadow-lg mb-4" href="#">
+                        <div class="card-body">
+                            <div class="media align-items-center">
+                                <img class="avatar avatar-xl mr-4" src="{{ asset('assets/img/profile.png') }}"
+                                    alt="Image Description">
+
+                                <div class="media-body">
+                                    <h3 class="text-hover-light text-white mb-1">My Account Settings</h3>
+                                    <span class="text-white">Click to Update your Password and Profile Record</span>
+                                </div>
+
+                                <div class="ml-2 text-right">
+                                    <i class="tio-chevron-right tio-lg text-white text-hover-primary"></i>
+                                </div>
+                            </div>
                         </div>
                     </a>
                 </div>
             </div>
         </div>
+        <div class="col-6">
+            <div class="card shadow-lg">
+                <div class="card-header">
+                    <h4 class="card-header-title">Latest transactions</h4>
+                    <div class="hs-unfold">
+                        <a class="js-hs-unfold-invoker btn btn-icon btn-sm btn-ghost-secondary rounded-circle"
+                            href="javascript:;" data-hs-unfold-options='{
+                                                       "target": "#reportsOverviewDropdown3",
+                                                       "type": "css-animation"
+                                                     }'>
+                            <i class="tio-more-vertical"></i>
+                        </a>
 
+                        <div id="reportsOverviewDropdown3"
+                            class="hs-unfold-content dropdown-unfold dropdown-menu dropdown-menu-right mt-1">
+                            <span class="dropdown-header">More Detail</span>
+
+                            <a class="dropdown-item" href="#">
+                                <i class="tio-file dropdown-item-icon"></i> Complete Detail
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body card-body-height">
+                    <ul class="list-group list-group-flush list-group-no-gutters">
+                        <li class="list-group-item">
+                            <div class="media">
+                                <img class="avatar avatar-sm avatar-circle mr-3"
+                                    src="{{ asset('assets/img/finance.png') }}" alt="Image Description">
+                                <div class="media-body">
+                                    <div class="row">
+                                        <div class="col-7 col-md-5 order-md-1">
+                                            <h5 class="mb-0 text-danger">Withdraw</h5>
+                                            <span class="font-size-sm">Net Income</span>
+                                        </div>
+
+                                        <div class="col-5 col-md-4 order-md-3 text-right mt-2 mt-md-0">
+                                            <h5 class="mb-0 text-danger">-$10.00 USD</h5>
+                                            <span class="font-size-sm">{{ date('Y-m-d') }}</span>
+                                        </div>
+
+                                        <div class="col-auto col-md-3 order-md-2">
+                                            <span class="badge badge-soft-warning badge-pill">Pending</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="media">
+                                <img class="avatar avatar-sm avatar-circle mr-3"
+                                    src="{{ asset('assets/img/finance.png') }}" alt="Image Description">
+                                <div class="media-body">
+                                    <div class="row">
+                                        <div class="col-7 col-md-5 order-md-1">
+                                            <h5 class="mb-0 text-success">Order Revenue</h5>
+                                            <span class="font-size-sm">Net Income</span>
+                                        </div>
+
+                                        <div class="col-5 col-md-4 order-md-3 text-right mt-2 mt-md-0">
+                                            <h5 class="mb-0 text-success">-$10.00 USD</h5>
+                                            <span class="font-size-sm">{{ date('Y-m-d') }}</span>
+                                        </div>
+
+                                        <div class="col-auto col-md-3 order-md-2">
+                                            <span class="badge badge-soft-success badge-pill">Complete</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
