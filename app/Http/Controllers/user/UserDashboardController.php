d27f31178bc032c8e41c52c383b9db53a9cdabc1
Shakeel Ahmad <shakeel2717@gmail.com>
2021-12-23 15:32:30 +0500

    User Dashboard Submit Request Section Design Created

--- app/Http/Controllers/user/UserDashboardController.php
+++ app/Http/Controllers/user/UserDashboardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class UserDashboardController extends Controller
 {
     public function index()
     {
-        return view('user.dashboard.index');
+        $categories = Category::where('status', 'active')->get();
+        return view('user.dashboard.index', compact('categories'));
     }
 }
--- resources/views/user/dashboard/index.blade.php
+++ resources/views/user/dashboard/index.blade.php
@@ -5,4 +5,57 @@
 @section('content')
     <h3 class="display-4">User Dashboard</h3>
     <hr>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Find a Service</h4>
+                </div>
+                <div class="card-body">
+                    <form action="#" method="POST">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="category">Select Category</label>
+                                    <select name="category" id="category"
+                                        class="js-select2-custom custom-select form-control">
+                                        @forelse ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->value }}</option>
+                                        @empty
+                                            <option value="">No Category Found</option>
+                                        @endforelse
+                                    </select>
+                                </div>
+                                <div class="form-group">
+                                    <label for="requirement">Requirements</label>
+                                    <textarea name="requirement" id="requirement" cols="30" rows="10" class="form-control"
+                                        placeholder="Tell us what you need and we will get it for you. eg. Blue shoe size 34"></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <label for="budget">Budgets</label>
+                                    <input type="text" name="budget" id="budget" class="form-control"
+                                        placeholder="Budget">
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block btn-lg">Submit your Request</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('footer')
+    <script>
+        $(document).on('ready', function() {
+            // INITIALIZATION OF SELECT2
+            // =======================================================
+            $('.js-select2-custom').each(function() {
+                var select2 = $.HSCore.components.HSSelect2.init($(this));
+            });
+        });
+    </script>
 @endsection
